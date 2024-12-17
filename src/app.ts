76b5e27974272b21fc0148dbbffc1e681d63e36a
Christian Nuss <christian@ybor.ai>
2024-12-17 13:15:47 -0500

    update router order

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', (req, res) => {
-  res.status(200).json({ healthy: true, version });
-});
-
 app.use('/.extensions/notify', (req, res) => {
   res.status(204).send();
+});
+
+// Always Last
+app.use('/', (req, res) => {
+  res.status(200).json({ healthy: true, version });
 });
 
 export default app;
