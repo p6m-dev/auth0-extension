007ad26405d38b9190d74831f6e67d9ef5f59318
Christian Nuss <christian@ybor.ai>
2024-12-19 09:32:03 -0500

    temp disable auth0 api

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,38 +2,38 @@ import express from 'express';
 import { version } from '../../webtask.json';
 import { identified } from '../auth/middleware';
 import { Context, RequestWithUserInfo } from '../types';
-import { ManagementClient, Client } from 'auth0';
+// import { ManagementClient, Client } from 'auth0';
 import { getClaim } from '../claims';
 
-const fetchClients = async (
-  auth0: ManagementClient,
-  orgId?: string,
-): Promise<Client[]> => {
-  //TODO: Pagination
-  const clients = await auth0.clients.getAll().then((c) => {
-    console.log('Response from auth0 api', JSON.stringify(c));
-    // Always filter!
-    //  - If no orgId, only get clients without an OrganizationId in metadata (global clients)
-    //  - Otherwise, make sure the provided OrganizationId matches
-    return c.data.filter((c) =>
-      !orgId
-        ? c.client_metadata['OrganizationId'] === undefined
-        : c.client_metadata['OrganizationId'] === orgId,
-    );
-  });
+// const fetchClients = async (
+//   auth0: ManagementClient,
+//   orgId?: string,
+// ): Promise<Client[]> => {
+//   //TODO: Pagination
+//   const clients = await auth0.clients.getAll().then((c) => {
+//     console.log('Response from auth0 api', JSON.stringify(c));
+//     // Always filter!
+//     //  - If no orgId, only get clients without an OrganizationId in metadata (global clients)
+//     //  - Otherwise, make sure the provided OrganizationId matches
+//     return c.data.filter((c) =>
+//       !orgId
+//         ? c.client_metadata['OrganizationId'] === undefined
+//         : c.client_metadata['OrganizationId'] === orgId,
+//     );
+//   });
 
-  return clients;
-};
+//   return clients;
+// };
 
 export default (ctx: Context) => {
   console.log('api route', ctx.meta);
 
-  const auth0 = new ManagementClient({
-    clientId: ctx.secrets.MANAGEMENT_CLIENT_ID || '',
-    clientSecret: ctx.secrets.MANAGEMENT_CLIENT_SECRET || '',
-    domain: ctx.secrets.AUTH0_DOMAIN || '',
-    audience: ctx.secrets.MANAGEMENT_AUDIENCE || '',
-  });
+  // const auth0 = new ManagementClient({
+  //   clientId: ctx.secrets.MANAGEMENT_CLIENT_ID || '',
+  //   clientSecret: ctx.secrets.MANAGEMENT_CLIENT_SECRET || '',
+  //   domain: ctx.secrets.AUTH0_DOMAIN || '',
+  //   audience: ctx.secrets.MANAGEMENT_AUDIENCE || '',
+  // });
 
   const router = express.Router();
 
@@ -58,8 +58,9 @@ export default (ctx: Context) => {
     const orgId = (Object.entries(orgs).find(([, name]) => name === org) ||
       [])[0];
 
-    const clients = await fetchClients(auth0, orgId);
-    res.status(200).json(clients);
+    // const clients = await fetchClients(auth0, orgId);
+    // res.status(200).json(clients);
+    res.status(200).json([]);
   });
 
   return router;
